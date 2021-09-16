d62d08b24ebc6683c8587d1e3fee08adf73e4e63
Anastasia Shatokhina <shnatya@gmail.com>
2021-09-16 13:26:46 -0700

    commit tree 4567d37858b113e0ef9639ec1427d9b3c9052c9c

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(nameUser, emailUser) {
+    return fetch("http://localhost:3000/users", {
+                 method: "POST",
+                 headers: {
+                     "Content-Type": 'application/json',
+                     Accept: "application/json"
+                 },
+                 body: JSON.stringify({
+                     name: nameUser,
+                     email: emailUser,
+                 })
+    })
+            .then(res => res.json())
+            .then(data => {
+                let id = document.createElement("h2")
+                id.textContent = data.id
+                document.body.appendChild(id)
+                
+            })
+            .catch(error => {
+                let er = document.createElement("h2")
+                er.textContent = error.message
+                document.body.appendChild(er)
+            }
+                )
+            
+            
+}
